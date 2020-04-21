d4d83c7def95cfd6cd91321f286b43df199b6a53
Brad Mehder <bmehder@gmail.com>
2020-04-21 23:34:57 +0200

    logged x value

--- eyes/script.js
+++ eyes/script.js
@@ -4,6 +4,7 @@ function eyeball() {
   let eye = document.querySelectorAll('.eye');
   eye.forEach((eye) => {
     let x = eye.getBoundingClientRect().left + eye.clientWidth / 2;
+    console.log(x);
     let y = eye.getBoundingClientRect().top + eye.clientHeight / 2;
     let radian = Math.atan2(event.pageX - x, event.pageY - y);
     let rot = radian * (180 / Math.PI) * -1 + 270;
