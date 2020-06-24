5d5157c0fcbe0a439c852d7656434d158439f10e
Brad Mehder <bmehder@gmail.com>
2020-06-24 19:41:39 +0200

    added ninja chat v1

---
+++ jschat/scripts/ui.js
@@ -0,0 +1,29 @@
+// render chat templates to the DOM
+// clear the list of chats (when the room changes)
+
+class ChatUI {
+  constructor(list) {
+    this.list = list;
+  }
+
+  clear() {
+    this.list.innerHTML = '';
+  }
+
+  render(data) {
+    const when = dateFns.distanceInWordsToNow(data.created_at.toDate(), {
+      addSuffix: true,
+    });
+
+    // const when = data.created_at.toDate();
+    const html = `
+      <li class="list-group-item">
+        <span class="username">${data.username}</span>
+        <span class="message">${data.message}</span>
+        <dive class="time">${when}</dive
+      </li>
+    `;
+
+    this.list.innerHTML += html;
+  }
+}
